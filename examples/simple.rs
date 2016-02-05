1ce23826885b36cc4383e36f21d41e3d938d7cc1
Dylan Ede <dylanede@googlemail.com>
2016-02-05 19:38:04 +0000

    Added more comments to the example. Improved variable names.

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,6 @@
 extern crate rusttype;
 
-use rusttype::{FontCollection, PixelsXY, point};
+use rusttype::{FontCollection, PixelsXY, point, PositionedGlyph};
 use std::io::Write;
 
 fn main() {
@@ -15,14 +15,18 @@ fn main() {
         // 2x scale in x direction to counter the aspect ratio of monospace characters.
         let scale = PixelsXY(height*2.0, height);
 
+        // The origin of a line of text is at the baseline (roughly where non-descending letters sit).
+        // We don't want to clip the text, so we shift it down with an offset when laying it out.
+        // v_metrics.ascent is the distance between the baseline and the highest edge of any glyph in
+        // the font. That's enough to guarantee that there's no clipping.
         let v_metrics = font.v_metrics(scale);
         let offset = point(0.0, v_metrics.ascent);
 
         // Glyphs to draw for "Hello!". Feel free to try other strings.
-        let to_draw: Vec<_> = font.layout("Hello!", scale, offset).collect();
+        let glyphs: Vec<PositionedGlyph> = font.layout("Hello!", scale, offset).collect();
 
         // Find the most visually pleasing width to display
-        let width = to_draw.iter().map(|g| g.h_metrics().advance_width)
+        let width = glyphs.iter().map(|g| g.h_metrics().advance_width)
             .fold(0.0, |x, y| x + y).ceil() as usize;
 
         println!("width: {}, height: {}", width, pixel_height);
@@ -31,7 +35,7 @@ fn main() {
         let mut pixel_data = vec![b'@'; width * pixel_height];
         let mapping = b"@%#x+=:-. "; // The approximation of greyscale
         let mapping_scale = (mapping.len()-1) as f32;
-        for g in to_draw {
+        for g in glyphs {
             if let Some(bb) = g.pixel_bounding_box() {
                 g.draw(|x, y, v| {
                     // v should be in the range 0.0 to 1.0
