72f0344779cc55e6b99f74e4c286930902111184
Dylan Ede <dylanede@googlemail.com>
2016-02-05 18:45:53 +0000

    Slightly tweaked example.

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,18 +3,19 @@ use rusttype::*;
 fn main() {
     use std::io::Write;
     let font_data = include_bytes!("Gudea-Regular.ttf");
-    let collection = FontCollection::<'static>::from_bytes(font_data as &[u8]);
+    let collection = FontCollection::from_bytes(font_data as &[u8]);
     let font = collection.into_font().unwrap(); // only succeeds if collection consists of one font
     {
         let height: f32 = 20.1; // to get 80 chars across (fits most terminals); adjust as desired
         let pixel_height = height.ceil() as usize;
+        // 2x scale in x direction to counter the aspect ratio of monospace characters.
         let scale = PixelsXY(height*2.0, height);
         let v_metrics = font.v_metrics(scale);
         let offset = point(0.0, v_metrics.ascent);
-        // 2x scale in x direction to counter the aspect ratio of monospace characters.
-        let to_draw: Vec<_> = font.layout("Hello!", PixelsXY(height*2.0, height), offset).collect();
+        let to_draw: Vec<_> = font.layout("Hello!", scale, offset).collect();
         // Finds the most visually pleasing width to display
-        let width = to_draw.iter().map(|g| g.h_metrics().advance_width).fold(0.0, |x, y| x + y).ceil() as usize;
+        let width = to_draw.iter().map(|g| g.h_metrics().advance_width)
+            .fold(0.0, |x, y| x + y).ceil() as usize;
         println!("width: {}, height: {}", width, pixel_height);
         // Rasterise directly into ASCII art.
         let mut data = vec![b'@'; width * pixel_height];
