a7fa8863a4e7d12f2bad9c31a7961e88e376fa49
Alex Butler <alexheretic@gmail.com>
2018-05-10 18:48:23 +0100

    Update dev-dependencies + minor tweaks

--- src/gpu_cache.rs
+++ src/gpu_cache.rs
@@ -1089,9 +1089,8 @@ mod cache_bench_tests {
         let mut last_glyph_id = None;
         for c in text.nfc() {
             if c.is_control() {
-                match c {
-                    '\n' => caret = point(0.0, caret.y + advance_height),
-                    _ => {}
+                if c == '\n' {
+                    caret = point(0.0, caret.y + advance_height)
                 }
                 continue;
             }
